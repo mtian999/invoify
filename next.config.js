5cc1b0e3e90763ff68795a0385ed8aafb0b4a536
tianbao <mtian999@163.com>
2025-08-23 17:35:24 +0800

    Revert "feat: 解决puppeteer问题，更新项目配置，添加Chromium依赖库路径，优化PDF生成服务"
    This reverts commit 430b4984cff07bb0721d03a9402432b8a95c9fe9.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+        serverComponentsExternalPackages: ["puppeteer-core"],
     },
     webpack: (config) => {
         config.module.rules.push({
